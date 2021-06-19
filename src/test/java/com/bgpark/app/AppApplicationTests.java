aec6d8b95492465b0c522c120d258d35720264b5
bgpark <bgpark82@gmail.com>
2021-06-19 16:45:38 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/bgpark/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgpark.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
