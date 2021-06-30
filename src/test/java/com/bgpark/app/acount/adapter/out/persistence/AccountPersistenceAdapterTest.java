8a6296328000bdfe9324288a9adc1480ab056c3d
bgpark <bgpark82@gmail.com>
2021-06-30 20:52:28 +0900

    test: Repository(Persistence Adapter) 통합 테스트

---
+++ src/test/java/com/bgpark/app/acount/adapter/out/persistence/AccountPersistenceAdapterTest.java
@@ -0,0 +1,57 @@
+package com.bgpark.app.acount.adapter.out.persistence;
+
+import com.bgpark.app.acount.domain.Account;
+import com.bgpark.app.acount.domain.Account.AccountId;
+import com.bgpark.app.acount.domain.ActivityWindow;
+import com.bgpark.app.acount.domain.Money;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+
+import static com.bgpark.app.common.AccountTestData.defaultAccount;
+import static com.bgpark.app.common.ActivityTestData.defaultActivity;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import({AccountPersistenceAdapter.class, AccountMapper.class})
+class AccountPersistenceAdapterTest {
+
+    @Autowired
+    private AccountPersistenceAdapter adapterUnderTest;
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @Test
+    @Sql("AccountPersistenceAdapterTest.sql")
+    void loadsAccount() {
+        Account account = adapterUnderTest.loadAccount(new AccountId(1L), LocalDateTime.of(2018, 8, 10, 0, 0));
+
+        assertThat(account.getActivityWindow().getActivities()).hasSize(2);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(500L));
+    }
+
+    @Test
+    void updatesActivities() {
+        Account account = defaultAccount()
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withId(null)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        adapterUnderTest.updateActivities(account);
+
+        assertThat(activityRepository.count()).isEqualTo(1);
+
+        ActivityJpaEntity savedActivity = activityRepository.findAll().get(0);
+        assertThat(savedActivity.getAmount()).isEqualTo(1L);
+    }
+
+}
