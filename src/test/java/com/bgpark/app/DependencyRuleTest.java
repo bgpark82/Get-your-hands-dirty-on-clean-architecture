60803b896bfde7845d2e4de4c3f9b8522bc5a283
bgpark <bgpark82@gmail.com>
2021-07-01 16:01:52 +0900

    test: DSL로 전체 아키텍쳐의 의존성 흐름 확인 테스트

--- src/test/java/com/bgpark/app/DependencyRuleTest.java
+++ src/test/java/com/bgpark/app/DependencyRuleTest.java
@@ -1,5 +1,6 @@
 package com.bgpark.app;
 
+import com.bgpark.app.archunit.HexagonalArchitecture;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,4 +20,28 @@ public class DependencyRuleTest {
                 .resideInAnyPackage("com.bgpark.app.account.application..")
                 .check(new ClassFileImporter().importPackages("com.bgpark.app.account.."));
     }
+
+    @DisplayName("DSL로 전체 아키텍쳐의 의존성 흐름 확인")
+    @Test
+    void validateRegistrationContextArchitecture() {
+        HexagonalArchitecture.boundedContext("com.bgpark.app.account")
+
+                .withDomainLayer("domain")
+
+                .withAdaptersLayer("adapter")
+                .incoming("in.web")
+                .outgoing("out.persistence")
+                .and()
+
+                .withApplicationLayer("application")
+                .services("service")
+                .incomingPorts("port.in")
+                .outgoingPorts("port.out")
+                .and()
+
+                .withConfiguration("configuration")
+                .check(new ClassFileImporter()
+                        .importPackages("com.bgpark.app.."));
+    }
+
 }
